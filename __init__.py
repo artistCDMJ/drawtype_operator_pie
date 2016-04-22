e680ca74b385b8d4fdecde91d59c07e86d3f7551
Craig Jones <artistcdmj@gmail.com>
2016-04-21 21:42:47 -0500

    Update __init__.py
    added addon name and category

--- __init__.py
+++ __init__.py
@@ -1,3 +1,40 @@
+# -*- coding: utf8 -*-
+# python
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+# <pep8 compliant>
+
+bl_info = {"name": "Drawtype Operator Pie",
+           "author": "CDMJ",
+           "version": (1, 0, 0),
+           "blender": (2, 77, 0),
+           "location": "",
+           "description": "Drawtype per object changing in 3d View",
+           "warning": "Not usable in TexPaint mode",
+           "category": "3D View"}
+
+
+
+
+
+
 import bpy
 from bpy.types import Menu
 
